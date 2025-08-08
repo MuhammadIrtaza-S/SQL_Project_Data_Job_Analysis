63cf5a8be7a84514024a240fed5355ec1d6b42db
Muhammad Irtaza <mughal9656964@gmail.com>
2025-08-08 13:29:33 -0700

    upload sql files

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -40,3 +40,4 @@ WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 COPY skills_job_dim
 FROM 'D:\SQL_Project_Data_Job_Analysis\csv_files\skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+
